81649381efcd36b6e7fc2c44503c2bcac6f7ce05
Kieran Downey <kdowney33@gmail.com>
2019-06-12 15:12:40 +0000

    Implemented authorisation functionality.

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from accounts.forms import UserLoginForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     """Return index.html file."""
     return render(request, 'index.html')
-    
+
+@login_required    
 def logout(request):
     """Log the user out."""
     auth.logout(request)
@@ -16,6 +18,8 @@ def logout(request):
 def login(request):
     """Return login page to the user."""
     #login_form = UserLoginForm() -- Used in previous lesson.
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
         if login_form.is_valid():
@@ -24,6 +28,7 @@ def login(request):
             if user:
                 auth.login(user=user, request=request)
                 messages.success(request, "You have successfully logged in!")
+                return redirect(reverse('index'))
             else:
                 login_form.add_error(None, "Your username or password is incorrect.")
     else:
